a6acba4569576e68ffcc3e8b482a0a17b53d58d3
Jacob Fenton <jsfenfen@gmail.com>
2019-10-28 13:50:22 -0700

    remove post gen hooks dependent on pipenv

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-command -v pipenv >/dev/null 2>&1 || { echo >&2 "pipenv not found.  Aborting startup script."; exit 1; }
-echo "PYTHONSTARTUP=$PWD/.startup.py" >> .env
-pipenv --three
-## ipykernel requires the latest prompt-toolkit library
-pipenv install "prompt-toolkit>=2.0.4"
-pipenv install ipykernel
-pipenv install pandas
-pipenv run python -m ipykernel install --user --name={{ cookiecutter.project_slug }}
+echo "Running commands from post_gen_project.sh"
+## Install stuff here via pip / pipenv if needed
+
+# pip install pandas
